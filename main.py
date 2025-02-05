e24d330d0663a3f2aaba67b9f5b383997c243d68
knjean26 <jean.pemika@gmail.com>
2025-02-05 10:03:57 +0700

    fix if ther is empty data

--- main.py
+++ main.py
@@ -568,26 +568,28 @@ def update_google_videononclickstats():
     client = GoogleAdsClient(credentials=credentials, developer_token=os.environ.get('GG_DEV_TOKEN'))
     all_data = GG_Connector.get_video_nonclickstat_data(client,customer_ids,query)
     df = pd.DataFrame(all_data)
-    df['segments_date'] = pd.to_datetime(df['segments_date'])
-    df['segments_month'] = pd.to_datetime(df['segments_month'])
-    df['segments_quarter'] = pd.to_datetime(df['segments_quarter'])
-    df['segments_week'] = pd.to_datetime(df['segments_week'])
     
-    df['segments_ad_network_type'] = df['segments_ad_network_type'].apply(GG_Connector.get_ad_network_type_description)
-    df['ad_group_ad_status'] = df['ad_group_ad_status'].apply(GG_Connector.get_ad_group_ad_status)
-    df['segments_day_of_week'] = df['segments_day_of_week'].apply(GG_Connector.get_day_of_week)
-    df['campaign_advertising_channel_type'] = df['campaign_advertising_channel_type'].apply(GG_Connector.get_advertising_channel_type)
-    
-    project_id = 'hmth-448709'
-    client = bigquery.Client(project=project_id)   
-    BQ_Connector.delete_data(client,"rda_analytics_temp","media_google_VideoNonClickStats_temp")
-    BQ_Connector.load_data(client, "rda_analytics_temp","media_google_VideoNonClickStats_temp",df)
-    condition = "ON (ori.segments_date = temp.segments_date AND ori.customer_id = temp.customer_id AND ori.ad_group_ad_ad_id = temp.ad_group_ad_ad_id AND ori.video_id = temp.video_id) "
-    BQ_Connector.delete_when_match(client,"rda_analytics","media_google_VideoNonClickStats","rda_analytics_temp","media_google_VideoNonClickStats_temp",condition)
-    BQ_Connector.load_data(client,"rda_analytics","media_google_VideoNonClickStats",df)
+    if len(df) > 0:
+        df['segments_date'] = pd.to_datetime(df['segments_date'])
+        df['segments_month'] = pd.to_datetime(df['segments_month'])
+        df['segments_quarter'] = pd.to_datetime(df['segments_quarter'])
+        df['segments_week'] = pd.to_datetime(df['segments_week'])
+        
+        df['segments_ad_network_type'] = df['segments_ad_network_type'].apply(GG_Connector.get_ad_network_type_description)
+        df['ad_group_ad_status'] = df['ad_group_ad_status'].apply(GG_Connector.get_ad_group_ad_status)
+        df['segments_day_of_week'] = df['segments_day_of_week'].apply(GG_Connector.get_day_of_week)
+        df['campaign_advertising_channel_type'] = df['campaign_advertising_channel_type'].apply(GG_Connector.get_advertising_channel_type)
+        
+        project_id = 'hmth-448709'
+        client = bigquery.Client(project=project_id)   
+        BQ_Connector.delete_data(client,"rda_analytics_temp","media_google_VideoNonClickStats_temp")
+        BQ_Connector.load_data(client, "rda_analytics_temp","media_google_VideoNonClickStats_temp",df)
+        condition = "ON (ori.segments_date = temp.segments_date AND ori.customer_id = temp.customer_id AND ori.ad_group_ad_ad_id = temp.ad_group_ad_ad_id AND ori.video_id = temp.video_id) "
+        BQ_Connector.delete_when_match(client,"rda_analytics","media_google_VideoNonClickStats","rda_analytics_temp","media_google_VideoNonClickStats_temp",condition)
+        BQ_Connector.load_data(client,"rda_analytics","media_google_VideoNonClickStats",df)
 
-    msg = "🌳 Media: <b>Google VideoNonClickStats</b> Executed Successfully on 📅 "
-    h_function.send_gg_chat_noti(msg)
+        msg = "🌳 Media: <b>Google VideoNonClickStats</b> Executed Successfully on 📅 "
+        h_function.send_gg_chat_noti(msg)
     return json.dumps({'success': 'Update Google VideoNonClickStats Completed'}), 200
 
 
@@ -728,8 +730,8 @@ def update_google_keywordcrossconversionstats():
     # Initialize the GoogleAdsClient with the credentials
     client = GoogleAdsClient(credentials=credentials, developer_token=os.environ.get('GG_DEV_TOKEN'))
     all_data = GG_Connector.get_keyword_cross_device_conversion_stat_data(client,customer_ids,query)
+    df = pd.DataFrame(all_data)
     if len(df) > 0:
-        df = pd.DataFrame(all_data)
         df['segments_date'] = pd.to_datetime(df['segments_date'])
         df['segments_month'] = pd.to_datetime(df['segments_month'])
         df['segments_quarter'] = pd.to_datetime(df['segments_quarter'])

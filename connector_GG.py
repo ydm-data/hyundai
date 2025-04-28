65234fa6142d0793e29fd6c7c3dd34bbb582baad
knjean26 <jean.pemika@gmail.com>
2025-04-28 13:21:53 +0700

    add customer login id

--- connector_GG.py
+++ connector_GG.py
@@ -425,21 +425,23 @@ class GG_Connector:
         if customer_id == '7498651531':
             service_account_info = json.loads(os.environ.get("hmth-bigquery"))
             developer_token=os.environ.get('GG_DEV_TOKEN')
+            login_customer_id = '4993931236'
         else:
             service_account_info = json.loads(os.environ.get("hmth-sem"))
             developer_token=os.environ.get('GG_DEV_TOKEN_SEM')
-        return service_account_info, developer_token
+            login_customer_id = '2685941715'
+        return service_account_info, developer_token, login_customer_id
                     
     def get_basicstat_data(customer_ids,query):
         all_data = []
         for customer_id in customer_ids:
             try:
                 scopes=['https://www.googleapis.com/auth/adwords']
-                service_account_info, developer_token = GG_Connector.get_service_dev_token(customer_id) 
+                service_account_info, developer_token,login_customer_id = GG_Connector.get_service_dev_token(customer_id) 
                 credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=scopes)
 
                 # Initialize the GoogleAdsClient with the credentials
-                client = GoogleAdsClient(credentials=credentials, developer_token=developer_token)
+                client = GoogleAdsClient(credentials=credentials, developer_token=developer_token,login_customer_id=login_customer_id)
                 # Execute the query for the current customer ID
                 ga_service = client.get_service("GoogleAdsService")
                 response = ga_service.search(customer_id=customer_id, query=query)
